16d12bb501e6077e1689d68bfce43180c0ac14cf
audi <audiaqsha578@gmail.com>
2021-03-31 18:39:05 +0700

    pertemuan10

---
+++ pertemuan10/latihan1.php
@@ -0,0 +1,47 @@
+<?php
+    include 'koneksi.php';
+?>
+
+<html>
+<head>
+
+</head>
+
+<body>
+
+<table border="1px solid black">
+    <tr>
+        <th>id</th>
+        <th>Foto</th>
+        <th>Nrp</th>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Jurusan</th>
+        <th>Aksi</th>
+    </tr>
+
+    
+
+    <?php foreach($mahasiswa as $mhs) {?>
+        <tr>
+            <th> <?=$mhs['id_mhs']?> </th>
+            <th><img src="img.jpg" style="width:100px;height:100px" alt=""></th>
+            <th><?=$mhs['nrp']?></th>
+            <th><?=$mhs['nama']?></th>
+            <th><?=$mhs['email']?></th>
+            <th><?=$mhs['jurusan']?></th>
+            <th>
+
+                <a href="#">Ubah</a>
+                <a href="#">Hapus</a>
+            
+            </th>
+        </tr>
+    <?php } ?>
+
+      
+</table>   
+
+</body>
+
+</html>
